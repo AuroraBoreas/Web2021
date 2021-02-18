8e3fe734429b2388092f20bb3963cb619f2b2d85
AuroraBoreas <AuroraBoreas@users.noreply.github.com>
2021-02-18 16:54:08 +0800

    for...in

--- JavaScript2021/c05_loop/js_01_for.js
+++ JavaScript2021/c05_loop/js_01_for.js
@@ -13,13 +13,23 @@ mechanism: is same with switchcase in C/C++ etc;
 
 */ 
 
+// for...
 for(let i=0; i<5; ++i)
 {
     console.log(i);
 }
 
+// for...of
 var strings = Array.from("hello world! ZL numba one")
 for(let s of strings)
 {
     console.log(s);
+}
+
+console.log();
+// for...in iterates a specified variable over all the enumerable properties of an objec
+var numbers = [1, 2, 3];
+for(let number in numbers)
+{
+    console.log(number)
 }
---
+++ JavaScript2021/c05_loop/js_03_dowhile.js
@@ -0,0 +1,7 @@
+var x = 4;
+
+do {
+    if(x==2){ continue; }
+    console.log(x);
+    x--;
+} while (x > 0);
